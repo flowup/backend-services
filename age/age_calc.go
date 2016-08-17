961780b628e745a0d0f6af287a0c33a9741112c4
Tomáš Hanák <tomashanak17@gmail.com>
2016-08-17 10:49:27 +0200

    docs(age): Checking and rewriting of all the comments

--- age/age_calc.go
+++ age/age_calc.go
@@ -2,7 +2,7 @@ package age
 
 import "time"
 
-// AgeService is a struct
+// AgeService is a type for the interface Service
 type AgeService struct {
 }
 
@@ -11,23 +11,8 @@ func NewAgeService() *AgeService {
 	return &AgeService{}
 }
 
-// CalculateAge is function calculating years from the given event to today
+// CalculateAge is function calculating time period from the given event to now
 func (s *AgeService) CalculateAge(event time.Time, now time.Time) time.Duration {
-
-	/*
-		ABANDONED: Precise calculation of years
-
-		now := time.Now()
-		years := now.Year() - event.Year()
-
-		//	Checks if event is going to happen this year but next month or in next days in current month
-		//	if so one year must be decremented
-		if now.Month() < event.Month() {
-			years--
-		} else if now.Month() == event.Month() && now.Day() < event.Day() {
-			years--
-		}
-	*/
 
 	duration := now.Sub(event)
 	return duration
--- age/age_calc_test.go
+++ age/age_calc_test.go
@@ -8,21 +8,25 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
+//	Array of testing dates that are used in TestCalculateAge
 var testingDates = [3]time.Time{
 	time.Date(2000, 3, 14, 0, 0, 0, 0, time.UTC),
 	time.Date(2010, 3, 14, 0, 0, 0, 0, time.UTC),
 	time.Date(2013, 8, 2, 0, 0, 0, 0, time.UTC),
 }
 
+//	Defining the suite
 type AgeServiceSuite struct {
 	suite.Suite
 }
 
+//	TestNewAgeService is testing if AgeService is created correctly
 func (s *AgeServiceSuite) TestNewAgeService() {
 	service := NewAgeService()
 	assert.NotEqual(s.T(), nil, service)
 }
 
+//	TestCalculateAge is testing calculating algorithm.
 func (s *AgeServiceSuite) TestCalculateAge() {
 	service := NewAgeService()
 	timeNow := time.Now()
@@ -32,6 +36,7 @@ func (s *AgeServiceSuite) TestCalculateAge() {
 	}
 }
 
+//	Passing of our suite (AgeServiceSuite) to suite.Run to be able to run the tests
 func TestAgeServiceSuite(t *testing.T) {
 	suite.Run(t, &AgeServiceSuite{})
 }
