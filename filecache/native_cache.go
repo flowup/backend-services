5e1083579daf2e3fea0bd9d8685aea9a948739c6
Tomáš Hanák <tomashanak17@gmail.com>
2016-09-14 11:16:40 +0200

    feat(Cache): Added method for changing already existing template + test for that feature.

--- filecache/native_cache.go
+++ filecache/native_cache.go
@@ -1,10 +1,10 @@
 package filecache
 
 import (
-  "os"
-  "runtime"
-  "strings"
-  "io/ioutil"
+	"io/ioutil"
+	"os"
+	"runtime"
+	"strings"
 )
 
 // instance of a cache
@@ -12,41 +12,48 @@ var Cache *NativeCache
 
 // NativeCache is a structure holding cached templates
 type NativeCache struct {
-  loadedTemplates map[string][]byte
+	loadedTemplates map[string][]byte
 }
 
 func init() {
-  Cache = &NativeCache{
-    loadedTemplates: make(map[string][]byte),
-  }
+	Cache = &NativeCache{
+		loadedTemplates: make(map[string][]byte),
+	}
 }
 
 // LoadFileNoCache will load a template and NOT save it in
 // cache.
 func (m *NativeCache) LoadFileNoCache(path string) []byte {
-  if runtime.GOOS == "windows" {
-    path = strings.Replace(path, "/", "\\", -1)
-  }
-  file, err := os.Open(path)
-  if err != nil {
-    panic(err)
-  }
-  defer file.Close()
-  bytes, err := ioutil.ReadAll(file)
-  if err != nil {
-    panic(err)
-  }
-  return bytes
+	if runtime.GOOS == "windows" {
+		path = strings.Replace(path, "/", "\\", -1)
+	}
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+	return bytes
 }
 
 // LoadTemplate will look into cache if the requested
 // template is not loaded already, if it is, it will return it
 // without any further ado, if it is not, it will load it,
 // save it to cache and return it.
-func (m *NativeCache) LoadFile(path string) []byte{
-  if bytes, ok := m.loadedTemplates[path]; ok == true {
-    return bytes
-  }
-  m.loadedTemplates[path] = m.LoadFileNoCache(path)
-  return m.loadedTemplates[path]
+func (m *NativeCache) LoadFile(path string) []byte {
+	if bytes, ok := m.loadedTemplates[path]; ok == true {
+		return bytes
+	}
+	m.loadedTemplates[path] = m.LoadFileNoCache(path)
+	return m.loadedTemplates[path]
+}
+
+// UpdateTemplate is updating existing template in map of templates
+func (m *NativeCache) UpdateTemplate(path string) []byte {
+
+	m.loadedTemplates[path] = m.LoadFileNoCache(path)
+	return m.loadedTemplates[path]
 }
--- filecache/native_cache_test.go
+++ filecache/native_cache_test.go
@@ -1,29 +1,65 @@
 package filecache
 
 import (
-  "github.com/stretchr/testify/suite"
-  "github.com/stretchr/testify/assert"
-  "testing"
-  "os"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 type ManagerSuite struct {
-  suite.Suite
+	suite.Suite
 
-  testFile string
+	testFile        string
+	testFileUpdated string
 }
 
-func (s *ManagerSuite)SetupSuite() {
-  s.testFile = os.Getenv("GOPATH") +
-    "/src/flowdock.eu/flowup/services/filecache/test_fixtures/template_test"
+func (s *ManagerSuite) SetupSuite() {
+	s.testFile = os.Getenv("GOPATH") +
+		"/src/flowdock.eu/flowup/services/filecache/test_fixtures/template_test"
+	s.testFileUpdated = os.Getenv("GOPATH") +
+		"/src/flowdock.eu/flowup/services/filecache/test_fixtures/template_test_updated"
 }
 
-func (s *ManagerSuite) TestCachingTemplates(){
-  readData := Cache.LoadFile(s.testFile)
-  assert.Equal(s.T(), "THIS IS A TEST TEMPLATE\n", string(readData))
+func (s *ManagerSuite) TestCachingTemplates() {
+	readData := Cache.LoadFile(s.testFile)
+	assert.Equal(s.T(), "THIS IS A TEST TEMPLATE NONE UPDATED\n", string(readData))
+}
+
+func (s *ManagerSuite) TestUpdatingTemplates() {
+	readData := Cache.LoadFile(s.testFile)
+	assert.Equal(s.T(), "THIS IS A TEST TEMPLATE NONE UPDATED\n", string(readData))
+
+	// Opening file and changing it
+	var file, err = os.OpenFile(s.testFile, os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString("THIS IS A TEST TEMPLATE YES UPDATED!\n")
+	if err != nil {
+		panic(err)
+	}
+
+	// Testing to LoadFile
+	readData = Cache.LoadFile(s.testFile)
+	assert.NotEqual(s.T(), "THIS IS A TEST TEMPLATE YES UPDATED!\n", string(readData))
+
+	// Testing of update function
+	readData = Cache.UpdateTemplate(s.testFile)
+	assert.Equal(s.T(), "THIS IS A TEST TEMPLATE YES UPDATED!\n", string(readData))
+
+	// Changing the Test Template to NONE UPDATED
+	_, err = file.WriteAt([]byte("THIS IS A TEST TEMPLATE NONE UPDATED"), 0)
+	if err != nil {
+		panic(err)
+	}
+	// save changes
+	err = file.Sync()
 }
 
 func TestManagerSuite(t *testing.T) {
-  suite.Run(t, &ManagerSuite{})
+	suite.Run(t, &ManagerSuite{})
 }
-
