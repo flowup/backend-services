373d937972031044225d3593f3e7c63c4018d08c
Tomáš Hanák <tomashanak17@gmail.com>
2016-09-14 13:05:24 +0200

    refactor(Cache): Some minor changes in tests, such as way of using test file.

--- filecache/native_cache_test.go
+++ filecache/native_cache_test.go
@@ -1,6 +1,7 @@
 package filecache
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -24,40 +25,41 @@ func (s *ManagerSuite) SetupSuite() {
 
 func (s *ManagerSuite) TestCachingTemplates() {
 	readData := Cache.LoadFile(s.testFile)
-	assert.Equal(s.T(), "THIS IS A TEST TEMPLATE NONE UPDATED\n", string(readData))
+	assert.Equal(s.T(), "THIS IS A TEST TEMPLATE\n", string(readData))
 }
 
 func (s *ManagerSuite) TestUpdatingTemplates() {
 	readData := Cache.LoadFile(s.testFile)
-	assert.Equal(s.T(), "THIS IS A TEST TEMPLATE NONE UPDATED\n", string(readData))
+	assert.Equal(s.T(), "THIS IS A TEST TEMPLATE\n", string(readData))
+
+	os.Truncate(s.testFile, 0)
 
 	// Opening file and changing it
 	var file, err = os.OpenFile(s.testFile, os.O_RDWR, 0644)
-	if err != nil {
-		panic(err)
-	}
+	assert.Nil(s.T(), err)
 	defer file.Close()
 
-	_, err = file.WriteString("THIS IS A TEST TEMPLATE YES UPDATED!\n")
-	if err != nil {
-		panic(err)
-	}
+	_, err = file.WriteString("UPDATED TEST TEMPLATE!\n")
+	assert.Nil(s.T(), err)
 
 	// Testing to LoadFile
 	readData = Cache.LoadFile(s.testFile)
-	assert.NotEqual(s.T(), "THIS IS A TEST TEMPLATE YES UPDATED!\n", string(readData))
+	fmt.Println(string(readData))
+	assert.NotEqual(s.T(), "UPDATED TEST TEMPLATE!\n", string(readData))
 
 	// Testing of update function
 	readData = Cache.UpdateTemplate(s.testFile)
-	assert.Equal(s.T(), "THIS IS A TEST TEMPLATE YES UPDATED!\n", string(readData))
+	fmt.Println(string(readData))
+	assert.Equal(s.T(), "UPDATED TEST TEMPLATE!\n", string(readData))
 
 	// Changing the Test Template to NONE UPDATED
-	_, err = file.WriteAt([]byte("THIS IS A TEST TEMPLATE NONE UPDATED"), 0)
-	if err != nil {
-		panic(err)
-	}
+
+	_, err = file.WriteAt([]byte("THIS IS A TEST TEMPLATE\n"), 0)
+
+	assert.Nil(s.T(), err)
 	// save changes
 	err = file.Sync()
+
 }
 
 func TestManagerSuite(t *testing.T) {
