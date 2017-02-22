d2964ea40f3c10a06b5e05284d4a6f91972eaf2e
walter <walter.scherfel@gmail.com>
2017-02-22 13:12:36 +0100

    refactor(email): Corrected commentary.

--- email/email.go
+++ email/email.go
@@ -1,7 +1,7 @@
 package email
 
-// Service is an interface for implementations
+// Sender is an interface for implementations
 // of email sending services
 type Sender interface {
-  Send(to []string, body []byte)
+	Send(to []string, body []byte)
 }
